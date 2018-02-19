918ac2e9723bd2307a6af8f491d36954aabd3d2c
ANISSA <anissamiloudi@hotmail.nl>
2018-02-19 20:08:03 +0100

    rankingmodel

--- models/evaluation.js
+++ models/evaluation.js
@@ -2,17 +2,28 @@
 const mongoose = require('../config/database')
 const { Schema } = mongoose
 
+
+const rankingSchema= new Schema({
+  studentId:  { type: Schema.Types.ObjectId, ref: 'users' },
+  rankDate:   { type: Date, required: true},
+  color:      { type: String, default: "Red"},
+  remarks:    { type: String}
+});
+
 const studentSchema = new Schema({
   studentId:  { type: Schema.Types.ObjectId, ref: 'users' },
   name:       { type: String, required: true },
   picture:    {type: String, required:true}
+
 });
 
 const batchSchema = new Schema({
   students: [studentSchema],
   class:      {type: Number, required:true},
-  startDate:  { type: Date },
-  endDate:    { type: Date },
+  startDate:  { type: Date, required: true},
+  endDate:    { type: Date, required: true},
 });
+
+
 
 module.exports = mongoose.model('batches', batchSchema)
